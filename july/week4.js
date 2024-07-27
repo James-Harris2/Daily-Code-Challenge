0b25a1f4ac659a7b613db6168a34e259cebda21f
James Harris <jameseharrisii@gmail.com>
2024-07-27 09:39:26 -0700

    added turtles function

--- july/week4.js
+++ july/week4.js
@@ -3,3 +3,21 @@
 function multiplyTwoNumbers(num1, num2){
     return num1 * num2
 }
+
+
+// Arrays, Indexes, 
+
+const fruit = ['apple', 'banana', 'orange', 'mango', 'apple', 'cherry']
+// each fruit is element
+// Indexes each element has Index (starts @ 0)
+
+fruit[2]
+
+const turtles = ['Raphael', 'Michelangelo', 'Leonardo', 'Donatello']
+
+function turtlesPower(turtles) {
+    return turtles[3]
+}
+
+
+fruit[2] = 'grapes'

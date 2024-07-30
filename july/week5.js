2834c41f7643ac746d502971475fae09661e8f27
James Harris <jameseharrisii@gmail.com>
2024-07-29 19:19:25 -0700

    add findPerimeter func

--- july/week5.js
+++ july/week5.js
@@ -50,3 +50,22 @@ function sumPolygon(n){
 
 console.log(sumPolygon(3))
 console.log(sumPolygon(4));
+
+
+/*
+Create a function that takes length
+ and width and finds the perimeter of a rectangle.
+findPerimeter(6, 7) ➞ 26
+
+findPerimeter(20, 10) ➞ 60
+
+findPerimeter(2, 9) ➞ 22
+
+*/
+
+function findPerimeter(length, width){
+    return (length + width) * 2;
+}
+
+console.log(findPerimeter(6, 7))
+console.log(findPerimeter(20,11))

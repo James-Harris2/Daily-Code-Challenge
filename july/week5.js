01eb9447280cc0ed112af387733fd864573bc96c
James Harris <jameseharrisii@gmail.com>
2024-07-30 18:25:08 -0700

    nextEdge func

--- july/week5.js
+++ july/week5.js
@@ -122,3 +122,23 @@ function lessThanOrEqualToZero(num) {
 console.log(lessThanOrEqualToZero(5))
 console.log(lessThanOrEqualToZero(0))
 console.log(lessThanOrEqualToZero(-2))
+
+/*
+Create a function that finds the maximum range of a triangle's third edge, 
+where the side lengths are all integers
+
+nextEdge(8, 10) ➞ 17
+
+nextEdge(5, 7) ➞ 11
+
+nextEdge(9, 2) ➞ 10
+
+*/
+
+function nextEdge(side1, side2){
+    return (side1 + side2) -1
+}
+
+console.log(nextEdge(8,10))
+console.log(nextEdge(5, 7))
+console.log(nextEdge(9,2))

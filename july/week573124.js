90296b404f8b127f59c1bd21576e6488e5f74405
James Harris <jameseharrisii@gmail.com>
2024-07-31 19:00:52 -0700

    Faster Algo

--- july/week573124.js
+++ july/week573124.js
@@ -33,3 +33,40 @@ function addUpTo(n){
     return n * (n + 1) / 2;
 }
 console.log(addUpTo(10))
+
+/*
+
+function addUpTo(n){
+    let total = 0;
+    for (let i = 1; i <= n; i++){
+        total += i
+    }
+    return total;
+}
+
+
+// Testing the speed and performance of the addUpTo ****Long func*****
+
+var t1 = performance.now();
+addUpTo(100000000);
+var t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`)
+
+*/
+
+
+
+// THis ALGO RUNS FASTER || MORE EFFICIENT
+
+
+/*
+function addUpTo(n){
+    return n * (n + 1) / 2;
+}
+
+var time1 = performance.now()
+addUpTo(100000000)
+var time2 = performance.now()
+console.log(`Time Elasped: ${(time2 - time1) / 1000} seconds.`)
+
+*/
